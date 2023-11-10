ff7ac906f19198e0116717a22ff38038c9d5b0e8
surafel21-tech <surafel332211@gmail.com>
2023-11-10 18:59:30 +0300

    Created app-1 and rendered it

---
+++ src/js/app-1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class ProductList extends React.Component{
+    render(){
+        return(
+            <div className="ui unstackable items">
+                Hello, friend! I am a basic React component.
+            </div>
+        );
+    }
+}
+
+ 
