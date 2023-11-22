e26d3cc4ee6dc07b4f4cc8f59c1d8ab012fb8fe5
surafel21-tech <surafel332211@gmail.com>
2023-11-22 09:16:40 +0300

    Sorting products the using array sort

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { ProductList } from './js/app-3';
-import { Product } from './js/app2';
+// import { ProductList } from './js/app-3';
+// import { ProductList } from './js/app-4';
+import { ProductList } from './js/app-5';
+import {Product } from './js/app-4';
+// import { Product } from './js/app2';
 
 function App() {
   return (
    <React.Fragment>
+    {/* <Product/> */}
     <ProductList/>
    </React.Fragment>
   );
