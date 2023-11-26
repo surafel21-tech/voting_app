c9850e1f8cfd681232b049792ff2baf3e346fd0e
surafel21-tech <surafel332211@gmail.com>
2023-11-26 22:16:43 +0300

    Handled the productUpVote click

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 // import { ProductList } from './js/app-5';
 // import { ProductList } from './js/app-6';
 // import { ProductList } from './js/app-7';
-import { ProductList } from './js/app-8';
+// import { ProductList } from './js/app-8';
+import { ProductList } from './js/app-9';
 import {Product } from './js/app-4';
 // import { Product } from './js/app2';
 
