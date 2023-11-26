2288637df5f530c7d7c6054a4d01b171cbf162ef
surafel21-tech <surafel332211@gmail.com>
2023-11-26 22:40:13 +0300

    Performed refactoring

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 // import { ProductList } from './js/app-6';
 // import { ProductList } from './js/app-7';
 // import { ProductList } from './js/app-8';
-import { ProductList } from './js/app-9';
+// import { ProductList } from './js/app-9';
+import { ProductList } from './js/all-complete';
 import {Product } from './js/app-4';
 // import { Product } from './js/app2';
 
